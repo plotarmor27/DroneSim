b0c2d5c89a6e127df19e4d31c8003cd8e110bc72
athee <atheesang@gmail.com>
2024-01-11 00:36:05 +0100

    Drone Getter and Setter added

--- src/Drone.java
+++ src/Drone.java
@@ -6,4 +6,44 @@ public class Drone {
     private String serial_number;
     private int carriage_weight;
     private String carriage_type;
+
+    // Getter
+    public static int get_id(){
+
+    }
+    public static String get_drone_type(){
+
+    }
+    public static LocalDateTime get_created(){
+
+    }
+    public static String get_serial_number(){
+
+    }
+    public static int get_carriage_weight(){
+
+    }
+    public static String get_carriage_type(){
+
+    }
+
+    // Setter
+    public static void set_id(){
+        id =
+    }
+    public static void set_drone_type(){
+        drone_type =
+    }
+    public static void set_created(){
+        created =
+    }
+    public static void set_serial_number(){
+        serial_number =
+    }
+    public static void set_carriage_weight(){
+        carriage_weight =
+    }
+    public static void set_carriage_type(){
+        carriage_type =
+    }
 }
