b25e98f431187d5677d23f8ce67267e0aa6ec45f
athee <atheesang@gmail.com>
2024-01-11 00:25:34 +0100

    Drone Attributes added

---
+++ src/Drone.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+public class Drone {
+    private int id;
+    private String drone_type;
+    private LocalDateTime created;
+    private String serial_number;
+    private int carriage_weight;
+    private String carriage_type;
+}
