525c266cdea2d98ff04db8022ffd004b39e4b8de
athee <atheesang@gmail.com>
2024-01-11 04:25:46 +0100

    dronemapper success

--- src/DroneMapper.java
+++ src/DroneMapper.java
@@ -4,16 +4,11 @@ import java.lang.Math;
 
 public class DroneMapper {
     private static final String drones_category = "drones";
-    public static void map_drones(JSONArray drones){
-        int i;
-        for (i = 0; i < drones.length(); i++){
-            System.out.println(drones.getJSONObject(i).toString());
-            map_drone(drones.getJSONObject(i));
-        }
-    }
+
     public static void main(String[] args) {
         map_drones(ApiAdapter.api_results(drones_category));
     }
+
     public static String format_date(String date){
         if (date.length() > 18) {
             String date_ending = String.valueOf(Math.round(Float.parseFloat(date.substring(18,20))));
@@ -31,5 +26,12 @@ public class DroneMapper {
         drone.set_serial_number(drone_json.getString("serialnumber"));
         drone.set_carriage_weight(drone_json.getInt("carriage_weight"));
         drone.set_carriage_type(drone_json.getString("carriage_type"));
+    }
+    public static void map_drones(JSONArray drones){
+        int i;
+        for (i = 0; i < drones.length(); i++){
+            System.out.println(drones.getJSONObject(i).toString());
+            map_drone(drones.getJSONObject(i));
+        }
     }
 }
